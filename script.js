6fdcf0958baf707486b6cdc8205a8a955854023c
Md.Abdur Rahman <ar.minhazul@gmail.com>
2024-09-13 23:55:48 +0600

    added some javaScript

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const randomNumber = Math.random();
-        let result;
-        function pickComputerMove(){
-            let computerMove = '';
-            if (randomNumber>=0 && randomNumber<1/3){
-                computerMove = 'rock';
-            }else if (randomNumber>=1/3 && randomNumber<2/3){
-                computerMove = 'paper';
-            }else if (randomNumber>=2/3 && randomNumber<1){
-                computerMove = 'scissors';
-            }
-            return computerMove;
-
+let result;
+function playGame(playerMove){
+    const computerMove = pickComputerMove();
+    if (playerMove==='rock'){  
+        if (computerMove === 'rock'){
+            result = 'Tie.';
+        }else if (computerMove === 'paper'){
+            result = 'You lose.';
+        }else if (computerMove === 'scissors'){
+            result = 'You win.';
         }
+    }else if (playerMove=== 'paper'){
+        if (computerMove === 'rock'){
+            result = 'You win';
+        }else if (computerMove === 'paper'){
+            result = 'Tie';
+        }else if (computerMove === 'scissors'){
+            result = 'You lose.';
+        }
+    }else if(playerMove ==='scissors'){
+        if (computerMove === 'rock'){
+            result = 'You lose';
+        }else if (computerMove === 'paper'){
+            result = 'You win.';
+        }else if (computerMove === 'scissors'){
+            result = 'Tie.';
+        }
+    }
+    alert(`You picked ${playerMove}, Computer picked ${computerMove}. \n ${result}`)
+}
+function pickComputerMove(){
+    let computerMove = '';
+    if (randomNumber>=0 && randomNumber<1/3){
+        computerMove = 'rock';
+    }else if (randomNumber>=1/3 && randomNumber<2/3){
+        computerMove = 'paper';
+    }else if (randomNumber>=2/3 && randomNumber<1){
+        computerMove = 'scissors';
+    }
+    return computerMove;
+
+}
