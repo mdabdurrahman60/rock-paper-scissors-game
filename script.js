21eac8a1b41356474aa4b0c775717e98d41304e5
Md.Abdur Rahman <ar.minhazul@gmail.com>
2024-09-15 01:00:09 +0600

    jscode updated

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
-const randomNumber = Math.random();
-let result;
+const score = {
+    wins : 0,
+    losses: 0,
+    ties: 0
+};
+function resetScore(){
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+};
 function playGame(playerMove){
     const computerMove = pickComputerMove();
-    if (playerMove==='rock'){  
+    let result ='' ;
+
+    if (playerMove ==='rock'){
         if (computerMove === 'rock'){
             result = 'Tie.';
         }else if (computerMove === 'paper'){
             result = 'You lose.';
         }else if (computerMove === 'scissors'){
             result = 'You win.';
-        }
-    }else if (playerMove=== 'paper'){
+        };
+
+    }else if (playerMove ==='paper'){
         if (computerMove === 'rock'){
-            result = 'You win';
+            result = 'You win.';
         }else if (computerMove === 'paper'){
-            result = 'Tie';
+            result = 'Tie.';
         }else if (computerMove === 'scissors'){
             result = 'You lose.';
-        }
+        };
+
     }else if(playerMove ==='scissors'){
         if (computerMove === 'rock'){
-            result = 'You lose';
+            result = 'You lose.';
         }else if (computerMove === 'paper'){
             result = 'You win.';
         }else if (computerMove === 'scissors'){
             result = 'Tie.';
-        }
-    }
-    alert(`You picked ${playerMove}, Computer picked ${computerMove}. \n ${result}`)
-}
+        };
+
+    };
+    if(result ==='You win.'){
+        score.wins +=1;
+    }else if(result === 'You lose.'){
+        score.losses +=1;
+    }else if(result === 'Tie.'){
+        score.ties +=1;
+    };
+  
+    alert(`You picked ${playerMove}, Computer picked ${computerMove}. ${result} \n Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties} `)
+};
+
+
+
+
+
 function pickComputerMove(){
+    const randomNumber = Math.random();
     let computerMove = '';
     if (randomNumber>=0 && randomNumber<1/3){
         computerMove = 'rock';
@@ -40,4 +67,4 @@ function pickComputerMove(){
     }
     return computerMove;
 
-}
+};
