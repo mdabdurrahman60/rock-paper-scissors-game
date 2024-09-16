a8d2f26288ff68962e23b2fde69b8a545a270397
Md.Abdur Rahman <ar.minhazul@gmail.com>
2024-09-16 19:26:56 +0600

    scoreBoard Added

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ let score = JSON.parse(localStorage.getItem('score')) || {
     losses: 0,
     ties: 0
 };
-function resetScore(){
-    score.wins = 0;
-    score.losses = 0;
-    score.ties = 0;
-};
+updateScoreElement()
+
 function playGame(playerMove){
     const computerMove = pickComputerMove();
-    let result ='' ;
+    let result;
 
     if (playerMove ==='rock'){
         if (computerMove === 'rock'){
@@ -48,12 +45,17 @@ function playGame(playerMove){
         score.ties +=1;
     };
     localStorage.setItem('score',JSON.stringify(score));
-    alert(`You picked ${playerMove}, Computer picked ${computerMove}. ${result} \n Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties} `)
+    updateScoreElement();
+    document.querySelector('.js-result').innerHTML = result;
+    document.querySelector('.js-moves').innerHTML = `You ${playerMove} - ${computerMove} Computer `;
+    
 };
 
 
 
-
+function updateScoreElement(){
+    document.querySelector('.js-score').innerHTML = ` Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties} `;
+}
 
 function pickComputerMove(){
     const randomNumber = Math.random();
